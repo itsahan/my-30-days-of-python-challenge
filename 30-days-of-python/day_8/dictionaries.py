dc108553d35b9ab262008151a6ead843f8e2e1e6
Your Name <youremail@example.com>
2026-02-07 16:50:04 +0700

    day_8

---
+++ 30-days-of-python/day_8/dictionaries.py
@@ -0,0 +1,48 @@
+# Day 8: 30 Days of python programming
+
+dog = {}
+dog.update({'Name' : 'Bahlil',
+            'Color' : 'Black', 
+            'Breed' : 'Xoloitzcuintli', 
+            'Age' : 49 
+            })
+print('Species details :', dog)
+
+# first_name, last_name, gender, age, marital status, skills, country, city and address 
+student = {
+    'first_name': 'Joko',
+    'last_name': 'Widodo',
+    'gender': 'Male',
+    'marital_status': True,
+    'skills': ['Manipulation', 'Debt', 'Corruption'],
+    'country': 'Indonesia',
+    'Address': 'Solo'
+    }
+print('Student(?) :', student)
+
+len_stud = len(student)
+print('Student Lenght :', len_stud)
+
+print(type(student['skills']))
+
+student['skills'].extend(['Term-limit evasion', 'Immortality']) 
+# use .extend([]) for more arguments, use .append() for only 1 argument
+print(student)
+
+keys = student.keys()
+values = student.values()
+print('Keys :', keys, '\nValues :', values)
+
+items = student.items()
+print('List of items :', items)
+
+del student['skills']
+print('skills' in student)
+
+del student
+try:
+    print(student)
+except NameError:
+    print('He is gone for good')
+
+# Day 7 Done!
